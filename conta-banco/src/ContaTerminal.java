dc6e6574072671d05d18de503faf72eb0613da94
Paulo Jonas <pjonas21@gmail.com>
2023-07-04 16:14:54 -0300

    conclusao desafio conta bancaria simples

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Ola, mundo!");
+        
+        var scan = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o numero da Conta: ");
+        Integer numero = scan.nextInt();
+
+        System.out.println("Por favor, digite o numero da Agencia: ");
+        String agencia = scan.next();
+
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scan.next();
+
+        System.out.println("Por favor, informe o saldo: ");
+        Double saldo = scan.nextDouble();
+
+        System.out.println("Ola " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agencia " + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " ja esta disponivel para saque");
+
+        scan.close();
     }
 }
